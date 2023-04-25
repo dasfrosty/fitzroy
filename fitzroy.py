2f4f4515383fd3f40fe33bfa79aa882f13d5a181
Niels Godfredsen <ngodfredsen@gmail.com>
2023-04-24 23:07:59 -0700

    deploy lambda with python dependency

--- fitzroy.py
+++ fitzroy.py
@@ -1,9 +1,13 @@
 # import boto3
 import os
-from botocore.vendored import requests
+import requests
 
 TEST_URL = os.environ["TEST_URL"]
 
 
 def lambda_handler(event, context):
     requests.get(TEST_URL)
+
+
+if __name__ == "__main__":
+    lambda_handler(None, None)
--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
-
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
-
     actions = ["sts:AssumeRole"]
   }
 }
@@ -16,21 +14,29 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+resource "local_file" "fitzroy-py" {
+  content_base64 = filebase64("fitzroy.py")
+  filename       = "dist/package/fitzroy.py"
+}
+
 data "archive_file" "lambda" {
+  depends_on = [local_file.fitzroy-py]
+
   type        = "zip"
-  source_file = "fitzroy.py"
-  output_path = "fitzroy_archive.zip"
+  source_dir  = "dist/package"
+  output_path = "dist/fitzroy_archive.zip"
+
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "fitzroy_archive.zip"
+  filename      = "dist/fitzroy_archive.zip"
   function_name = "fitzroy_lambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "fitzroy.lambda_handler"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "python3.7"
+  runtime = "python3.9"
 
   environment {
     variables = {

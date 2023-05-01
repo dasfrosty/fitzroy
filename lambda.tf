f0c01fdc765d0542efd77679e7c25433016ca42f
Niels Godfredsen <ngodfredsen@gmail.com>
2023-04-30 22:10:00 -0700

    break out separate tf files for iam and lambda

--- lambda.tf
+++ lambda.tf
@@ -1,19 +1,3 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
 resource "aws_lambda_function" "fitzroy_lambda" {
   filename      = "dist/fitzroy_lambda.zip"
   function_name = "fitzroy_lambda"

d0940b892bd0a55342cf93a909498353e6f908d5
Niels Godfredsen <ngodfredsen@gmail.com>
2023-05-01 14:19:15 -0700

    add zapier subscription to sns topic

--- sns.tf
+++ sns.tf
@@ -7,3 +7,9 @@ resource "aws_sns_topic_subscription" "fitzroy_notifications_subscription" {
   protocol  = "email"
   endpoint  = var.notification_email_address
 }
+
+resource "aws_sns_topic_subscription" "fitzroy_zapier_subscription" {
+  topic_arn = aws_sns_topic.fitzroy_notifications_topic.arn
+  protocol  = "email"
+  endpoint  = var.zapier_email_address
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "notification_email_address" {
   type     = string
   nullable = false
 }
+
+variable "zapier_email_address" {
+  type     = string
+  nullable = false
+}

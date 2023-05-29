ede29f248b769b8d8389b639fe2087d27728df7e
Niels Godfredsen <ngodfredsen@gmail.com>
2023-05-29 12:41:53 -0700

    add cloudwatch logging permissions to lambda

--- iam.tf
+++ iam.tf
@@ -9,11 +9,20 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "sns_publish" {
+data "aws_iam_policy_document" "policy_for_lambda" {
   statement {
-    effect   = "Allow"
-    actions  = ["sns:Publish"]
+    effect    = "Allow"
+    actions   = ["sns:Publish"]
     resources = [aws_sns_topic.fitzroy_notifications_topic.arn]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
   }
 }
 
@@ -21,7 +30,7 @@ resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
   inline_policy {
-    name   = "sns_publish"
-    policy = data.aws_iam_policy_document.sns_publish.json
+    name   = "fitzroy_lambda_execution_policy"
+    policy = data.aws_iam_policy_document.policy_for_lambda.json
   }
 }
